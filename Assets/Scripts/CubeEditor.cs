d9ec4a46ebffecd14e7d57e8e606499a9e3227b7
telgerot <edward.k.robe@gmail.com>
2019-01-08 07:37:11 +0200

    First commit

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    TextMesh textMesh;
+    Vector3 snapPos;
+    Waypoint waypoint;
+    int gridSize;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+        gridSize = waypoint.GetGridSize();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3(waypoint.GetGridPos().x, 0f, waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = (waypoint.GetGridPos().x / gridSize) + "," + (waypoint.GetGridPos().y / gridSize);
+        textMesh.text = labelText;
+        gameObject.name = "Waypoint " + labelText;
+    }
+}
---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    [SerializeField] float movementPerSecond = 1f;
+
+    private void Start()
+    {
+        StartCoroutine (FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting block: " + waypoint.name);
+            yield return new WaitForSeconds(movementPerSecond);
+        }
+        print("Ending patrol");
+    }
+}
